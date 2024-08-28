324bbaf3f28018f9fcdfec60aa290fab8126bb10
lukasz-mac <luka.koscik@gmail.com>
2024-08-28 15:17:53 +0200

    change vite from react/js to react/ts, remove boilerplate

--- server/app.js
+++ server/app.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const cors = require('cors');
+
+const corsOptions = {
+    origin:['http://localhost:5173'],
+}
 
 const app = express();
+app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
+
+app.get('/api', (req, res) => {
+    res.json([{name: "my name"}, {name: "second name"}]);
+});
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
-})
+});
 
 
