f20d92eb6065bd39a674262bc325ee318df8d386
lukasz-mac <luka.koscik@gmail.com>
2024-09-07 18:21:05 +0200

    update layout

--- client/src/views/layout/layout.tsx
+++ client/src/views/layout/layout.tsx
@@ -1,11 +1,12 @@
-import Header from '@/views/header/header.tsx';
+import Header from '@views/header/header.tsx';
 import {Outlet} from "react-router-dom";
+import '@views/layout/layout.scss';
 
 const Layout = () => {
     return (
         <div>
             <Header/>
-            <main>
+            <main className="layout__main">
                 <Outlet/>
             </main>
         </div>
