bbe7448b716fa00bbbcb6b038edcfa72905af50f
lukasz-mac <luka.koscik@gmail.com>
2024-09-07 19:36:30 +0200

    add errors to input component

--- client/src/components/input/input.tsx
+++ client/src/components/input/input.tsx
@@ -1,16 +1,21 @@
-import './input.scss';
+import '@components/input/input.scss';
 import {InputHTMLAttributes} from "react";
 import clsx from "clsx";
 
-type InputPros = { labelName?: string } & InputHTMLAttributes<HTMLInputElement>;
+type InputPros = { labelName?: string, errors?: string[] } & InputHTMLAttributes<HTMLInputElement>;
 
 
-const Input = ({type, className, name, labelName, ...rest}: InputPros) => {
+const Input = ({type, className, name, labelName, errors, ...rest}: InputPros) => {
     return (
         <div className="input-wrapper">
             <label htmlFor={name} className="input-wrapper__label">{labelName || 'Label'}</label>
             <input type={type} name={name} className={clsx('input-wrapper__input', className)} {...rest}>
             </input>
+            {!!errors?.length && <ul className="input-wrapper__error-wrapper">
+                {errors.map(error => {
+                    return <li className="input-wrapper__error">{error}</li>;
+                })}
+            </ul>}
         </div>
     );
 };
