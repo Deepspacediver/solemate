4822cd12b5688251cb0d5c5bef8d9c2d55e7b494
lukasz-mac <luka.koscik@gmail.com>
2024-09-07 19:03:43 +0200

    add textarea component

---
+++ client/src/components/textarea/textarea.tsx
@@ -0,0 +1,25 @@
+import '@components/textarea/textarea.scss';
+
+import {TextareaHTMLAttributes} from "react";
+import clsx from "clsx";
+
+type InputPros = { labelName?: string, errors?: string[] } & TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+
+const Textarea = ({className, name, labelName, errors, ...rest}: InputPros) => {
+    return (
+        <div className="textarea-wrapper">
+            <label htmlFor={name} className="textarea-wrapper__label">{labelName || 'Label'}</label>
+            <textarea name={name} className={clsx('textarea-wrapper__textarea', className)} {...rest}>
+            </textarea>
+            {!!errors?.length && <ul className="textarea-wrapper__error-wrapper">
+                {errors.map(error => {
+                    return <li className="textarea-wrapper__error">{error}</li>;
+                })}
+            </ul>}
+        </div>
+    );
+};
+
+
+export default Textarea;
