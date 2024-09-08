a39f89d43ce9bb25bdb069fe4c14cf885f8368d0
lukasz-mac <luka.koscik@gmail.com>
2024-09-08 15:15:31 +0200

    add link variation to button component

--- client/src/components/button/button.tsx
+++ client/src/components/button/button.tsx
@@ -1,14 +1,20 @@
 import '@components/button/button.scss';
 import {ButtonHTMLAttributes, ReactNode,} from "react";
 import clsx from "clsx";
+import {NavLink} from "react-router-dom";
 
-type ButtonPros = { children: ReactNode } & ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonPros = { isNavlink?: boolean, path?: string, children: ReactNode }
+    & ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({children, className, ...rest}: ButtonPros) => {
+const Button = ({isNavlink, path, children, className, ...rest}: ButtonPros) => {
     return (
-        <button {...rest} className={clsx('button', className)}>
-            {children}
-        </button>
+        isNavlink && path ?
+            (<NavLink className={clsx('button', className)} to={path}>
+                {children}
+            </NavLink>) :
+            (<button {...rest} className={clsx(className, 'button')}>
+                {children}
+            </button>)
     );
 };
 
