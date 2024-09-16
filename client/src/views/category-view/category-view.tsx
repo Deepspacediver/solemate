730f269e987c0991c635397062a4ca852613e7e9
lukasz-mac <luka.koscik@gmail.com>
2024-09-16 11:36:58 +0200

    add basic category view

---
+++ client/src/views/category-view/category-view.tsx
@@ -0,0 +1,72 @@
+import '@views/category-view/category-view.scss';
+import {useEffect, useState} from "react";
+import {getCategoryWithShoes} from "@/services/category-services.ts";
+import {useParams} from "react-router-dom";
+import {CategoryWithShoesAPIType} from "@/types/category-types.ts";
+import PreviewItem from "@components/preview-item/preview-item.tsx";
+import ItemWrapper from "@components/item-wrapper/item-wrapper.tsx";
+
+
+const CategoryView = () => {
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [categoryWithShoes, setCategoryWithShoes] = useState<CategoryWithShoesAPIType | null>(null);
+    const {categoryId} = useParams();
+    const parsedCategoryId = categoryId ? +categoryId : null;
+
+    useEffect(() => {
+        const abortController = new AbortController();
+        const getShoesFromCategory = async () => {
+            if (!parsedCategoryId) return;
+
+            try {
+                setIsLoading(true);
+                const data = await getCategoryWithShoes(parsedCategoryId, abortController.signal);
+                setCategoryWithShoes(data);
+            } catch (err) {
+                console.error(err);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        getShoesFromCategory();
+
+        return () => {
+            abortController.abort();
+        };
+
+    }, []);
+
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+    const {
+        name: categoryName,
+        description: categoryDescription
+    } = categoryWithShoes?.category ?? {};
+    const shoesArray = categoryWithShoes?.shoes ?? [];
+
+    return (
+        <div className="category-view">
+            <h2 className="category-view__heading">{categoryName ?? ''}</h2>
+            <p className="category-view__description">{categoryDescription ?? ''}</p>
+
+            <ItemWrapper>
+                {!!shoesArray.length && shoesArray.map(({
+                                                            shoe_id,
+                                                            picture,
+                                                            name
+                                                        }) => {
+                    return <PreviewItem key={shoe_id} path={`/shoes/${shoe_id}`}
+                                        name={name} picture={picture}/>;
+                })}
+
+            </ItemWrapper>
+
+        </div>
+    );
+};
+
+export default CategoryView;
