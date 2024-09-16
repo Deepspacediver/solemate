174004c3cd5d39e36945bbbe88da0f8355527b14
lukasz-mac <luka.koscik@gmail.com>
2024-09-16 17:53:40 +0200

    update category update service & add getter for single category

--- client/src/services/category-services.ts
+++ client/src/services/category-services.ts
@@ -1,11 +1,17 @@
 import axiosClient from "@/services/index.ts";
-import {CategoryAPIType, CategoryType, CategoryWithShoesAPIType, CreateCategoryType} from "@/types/category-types.ts";
+import {
+    CategoryAPIType,
+    CategoryType,
+    CategoryWithShoesAPIType,
+    CreateCategoryType
+} from "@/types/category-types.ts";
 import {ShoeAPIType} from "@/types/shoe-types.ts";
 
 enum CategoryRoutes {
     CATEGORIES = "/categories",
     WITHOUT_CATEGORY = '/categoryless/list',
-    WITH_SHOES = '/category-with-shoes'
+    WITH_SHOES = '/category-with-shoes',
+    CATEGORY_DATA = '/category-data',
 }
 
 export const getAllCategories = async (signal: AbortSignal) => {
@@ -13,13 +19,31 @@ export const getAllCategories = async (signal: AbortSignal) => {
     return data;
 };
 
-export const createCategory = async ({name, picture, description}: CreateCategoryType) => {
-    const {data} = await axiosClient.post(`${CategoryRoutes.CATEGORIES}/category/add`, {name, picture, description});
+export const getCategory = async (categoryId: number, signal: AbortSignal) => {
+    const {data} = await axiosClient.get(`${CategoryRoutes.CATEGORIES}${CategoryRoutes.CATEGORY_DATA}/${categoryId}`, {signal});
     return data;
 };
 
-export const updateCategory = async ({name, picture, description, categoryId}: CategoryType) => {
-    const {data} = await axiosClient.put(`${CategoryRoutes.CATEGORIES}/:${categoryId}`, {
+export const createCategory = async ({
+                                         name,
+                                         picture,
+                                         description
+                                     }: CreateCategoryType) => {
+    const {data} = await axiosClient.post(`${CategoryRoutes.CATEGORIES}/category/add`, {
+        name,
+        picture,
+        description
+    });
+    return data;
+};
+
+export const updateCategory = async ({
+                                         name,
+                                         picture,
+                                         description,
+                                         categoryId
+                                     }: CategoryType) => {
+    const {data} = await axiosClient.put(`${CategoryRoutes.CATEGORIES}/${categoryId}`, {
         name,
         picture,
         description,
