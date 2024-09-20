74fd40c2011f8937e5529ba37f2cc8d67d5fb087
lukasz-mac <luka.koscik@gmail.com>
2024-09-20 19:13:15 +0200

    modify layout styles & add footer

--- client/src/views/layout/layout.tsx
+++ client/src/views/layout/layout.tsx
@@ -1,14 +1,16 @@
 import Header from '@views/header/header.tsx';
 import {Outlet} from "react-router-dom";
+import Footer from "@components/footer/footer.tsx";
 import '@views/layout/layout.scss';
 
 const Layout = () => {
     return (
-        <div>
+        <div className="layout">
             <Header/>
             <main className="layout__main">
                 <Outlet/>
             </main>
+            <Footer/>
         </div>
     );
 };
