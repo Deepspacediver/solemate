5c49125d8df91d6fd477567db39e5109a5e6ebbb
lukasz-mac <luka.koscik@gmail.com>
2024-09-20 19:11:39 +0200

    add footer component

---
+++ client/src/components/footer/footer.tsx
@@ -0,0 +1,28 @@
+import '@components/footer/footer.scss';
+import CustomNavlink from "@components/custom-navlink/custom-navlink.tsx";
+import githubSVG from '@/assets/icons/github-mark.svg';
+
+
+const gitHubLink = "https://github.com/Deepspacediver/solemate";
+const freepikLink = "https://www.freepik.com/";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <span className="footer__credit-wrapper">
+            <p className="footer__paragraph">Create by Deepscapediver </p>
+            <CustomNavlink className="footer__link footer__link--with-icon"
+                           path={gitHubLink}>
+                <img alt={'github project'} className="footer__icon"
+                     src={githubSVG}/>
+            </CustomNavlink>
+                </span>
+
+            <p>Images used from <CustomNavlink name={'freepik'}
+                                               path={freepikLink}/>
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
