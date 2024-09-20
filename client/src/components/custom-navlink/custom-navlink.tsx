7ff7e6254d3dd27a650b07f97abc6328ed29e090
lukasz-mac <luka.koscik@gmail.com>
2024-09-20 19:12:56 +0200

    add children to navlink

--- client/src/components/custom-navlink/custom-navlink.tsx
+++ client/src/components/custom-navlink/custom-navlink.tsx
@@ -1,16 +1,24 @@
 import {NavLink} from 'react-router-dom';
 import clsx from 'clsx';
 import './custom-navlink.scss';
+import {ReactNode} from "react";
 
 type CustomNavLinkPros = {
-    name: string
+    name?: string
     path: string
-    className?: string
+    className?: string,
+    children?: ReactNode
 }
 
-const CustomNavlink = ({name, path, className}: CustomNavLinkPros) => {
+const CustomNavlink = ({
+                           name,
+                           path,
+                           className,
+                           children
+                       }: CustomNavLinkPros) => {
     return (
-        <NavLink className={clsx('navlink', className)} to={path}>{name}</NavLink>
+        <NavLink className={clsx('navlink', className)}
+                 to={path}>{name ?? children}</NavLink>
     );
 };
 
