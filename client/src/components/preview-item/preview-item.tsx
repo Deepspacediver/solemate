5827de99fbe742b666e6fa004f0c2549b4fc4d33
lukasz-mac <luka.koscik@gmail.com>
2024-09-23 20:52:37 +0200

    [EXPERIMENTAL] add skeleton background to loading image

--- client/src/components/preview-item/preview-item.tsx
+++ client/src/components/preview-item/preview-item.tsx
@@ -1,6 +1,8 @@
 import {NavLink} from "react-router-dom";
-import previewImagePlaceholderSrc from "@/assets/images/category-placeholder.png";
+import previewImagePlaceholderSrc
+    from "@/assets/images/category-placeholder.png";
 import '@components/preview-item/preview-item.scss';
+import {useRef} from "react";
 
 type PreviewItemProps = {
     path: string,
@@ -10,12 +12,20 @@ type PreviewItemProps = {
 }
 
 const PreviewItem = ({path, picture, name}: PreviewItemProps) => {
+    const imageRef = useRef<HTMLImageElement | null>(null);
     return (
         <article className="preview-item">
             <NavLink className="preview-item__link" to={path}>
                 <div className="preview-item__image-wrapper">
-                    <img className="preview-item__image"
-                         src={picture || previewImagePlaceholderSrc}
+                    <img onLoad={() => {
+                        if (!imageRef.current) {
+                            return;
+                        }
+                        imageRef.current.classList?.remove("preview-item__image--loading");
+                    }}
+                         className="preview-item__image preview-item__image--loading"
+                         src={previewImagePlaceholderSrc || picture}
+                         ref={imageRef}
                          alt={name}/>
                 </div>
                 <div className="preview-item__info-wrap">
