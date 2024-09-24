570212b8ca73257792f6b83e327e45e40058305e
lukasz-mac <luka.koscik@gmail.com>
2024-09-24 16:08:01 +0200

    add background to home image and update styles

--- client/src/views/home-view/home-view.tsx
+++ client/src/views/home-view/home-view.tsx
@@ -3,9 +3,14 @@ import '@views/home-view/home-view.scss';
 import Button from "@components/button/button.tsx";
 
 const HomeView = () => {
+
     return (
         <div className="home-view">
-            <img className="home-view__image" src={heroImageSrc} alt=""/>
+            <div
+                className="home-view__image-wrapper home-view__image-wrapper--loading">
+                <img className="home-view__image" src={heroImageSrc}
+                     alt="white sneakers on feet"/>
+            </div>
             <div className="home-view__info">
                 <h2>Welcome to Solemate</h2>
                 <p>Explore shoes and its categories</p>
