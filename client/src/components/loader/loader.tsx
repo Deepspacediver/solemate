d60ab6d97aef5bd64a0107e55288c3e7d083be83
lukasz-mac <luka.koscik@gmail.com>
2024-09-25 16:28:30 +0200

    change loader svg attributes

--- client/src/components/loader/loader.tsx
+++ client/src/components/loader/loader.tsx
@@ -5,8 +5,8 @@ const LoaderSVG = ({className}: { className: string }) => <svg
     xmlns="http://www.w3.org/2000/svg" width="24"
     height="24"
     viewBox="0 0 24 24" fill="none" stroke="#000000"
-    stroke-width="2" stroke-linecap="round"
-    stroke-linejoin="round">
+    strokeWidth="2" strokeLinecap="round"
+    strokeLinejoin="round">
     <path d="M21 12a9 9 0 11-6.219-8.56"/>
 </svg>;
 
