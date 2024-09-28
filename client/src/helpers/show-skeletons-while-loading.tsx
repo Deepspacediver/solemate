7133bd978a7540eadb1ae11806f6a32f2d26f464
lukasz-mac <luka.koscik@gmail.com>
2024-09-28 19:18:10 +0200

    add optional parameter to showSkeletonsWhileLoading helper

--- client/src/helpers/show-skeletons-while-loading.tsx
+++ client/src/helpers/show-skeletons-while-loading.tsx
@@ -1,6 +1,6 @@
 import SkeletonPreview from "@components/skeleton-preview/skeleton-preview.tsx";
 
-export const showSkeletonsWhileLoading = () => {
-    return Array.from({length: 8}).map((_, index) =>
+export const showSkeletonsWhileLoading = (arrayLength = 8) => {
+    return Array.from({length: arrayLength}).map((_, index) =>
         <SkeletonPreview key={index}/>);
 };
