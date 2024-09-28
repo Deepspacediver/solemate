566e58f6bf9db8f78594831aa47e85d731386262
lukasz-mac <luka.koscik@gmail.com>
2024-09-28 19:20:38 +0200

    implement fetch on load for all shoes view

--- client/src/views/shoes-view/shoes-view.tsx
+++ client/src/views/shoes-view/shoes-view.tsx
@@ -1,25 +1,39 @@
 import '@views/shoes-view/shoes-view.scss';
 import Button from "@components/button/button.tsx";
 import {getAllShoes} from "@/services/shoe-services.ts";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {ShoeAPIType} from "@/types/shoe-types.ts";
 import PreviewItem from "@components/preview-item/preview-item.tsx";
 import ItemWrapper from "@components/item-wrapper/item-wrapper.tsx";
 import {
     showSkeletonsWhileLoading
 } from "@/helpers/show-skeletons-while-loading.tsx";
+import useFetchOnScroll from "@/hooks/use-intersection-observer.tsx";
 
 
 const ShoesView = () => {
     const [shoes, setShoes] = useState<ShoeAPIType[]>([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [lastShoeId, setLastShoeId] = useState<number | undefined>();
+    const [areAllShoesFetched, setAreAllShoesFetched] = useState(true);
+
+    const lastShoeHTLMItemRef = useRef<HTMLElement>(null);
+
+    const shoesFetchLimit = 15;
 
     useEffect(() => {
+        if (!areAllShoesFetched) {
+            return;
+        }
         const controller = new AbortController();
         const getShoes = async () => {
             try {
-                const shoesData = await getAllShoes(controller.signal);
-                setShoes(shoesData);
+                setIsLoading(true);
+                const shoesData = await getAllShoes(controller.signal, lastShoeId);
+                if (shoesData.length < shoesFetchLimit) {
+                    setAreAllShoesFetched(false);
+                }
+                setShoes((prevShoes) => [...prevShoes, ...shoesData]);
 
             } catch (err) {
                 console.error(err);
@@ -34,8 +48,20 @@ const ShoesView = () => {
             controller.abort();
         };
 
-    }, []);
+    }, [lastShoeId]);
 
+    const onIntersecting = () => {
+        if (!shoes.length) {
+            return;
+        }
+        setLastShoeId(shoes[shoes.length - 1]?.shoe_id ?? null);
+    };
+
+
+    useFetchOnScroll({
+        observedHTMLElement: lastShoeHTLMItemRef,
+        intersectingCallback: onIntersecting,
+    });
 
     return (
         <div className="shoes-view">
@@ -43,12 +69,17 @@ const ShoesView = () => {
             <Button className="shoes-view__button" isNavlink path={'add-shoe'}>Add
                 shoe</Button>
             <ItemWrapper>
-                {isLoading && showSkeletonsWhileLoading()}
-                {!!shoes.length && shoes.map(({shoe_id, name, picture}) => (
+                {!!shoes.length && shoes.map(({
+                                                  shoe_id,
+                                                  name,
+                                                  picture
+                                              }, index) => (
                     <PreviewItem key={shoe_id} name={name} picture={picture}
-                                 path={`${shoe_id}`}/>))}
+                                 path={`${shoe_id}`}
+                                 ref={index === shoes.length - 1 ? lastShoeHTLMItemRef : null}
+                    />))}
+                {isLoading && showSkeletonsWhileLoading(5)}
             </ItemWrapper>
-
         </div>
     );
 };
