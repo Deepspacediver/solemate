c6c4fcaad1c2e72ba9d9e26e04ea11cf3de657f1
lukasz-mac <luka.koscik@gmail.com>
2024-09-29 18:00:26 +0200

    add backend api host env for railway

--- client/src/services/index.ts
+++ client/src/services/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:5001',
+    baseURL: import.meta.env.VITE_BACKEND_HOST || 'http://localhost:5001',
     headers: {
         "Content-Type": "application/json",
     }
