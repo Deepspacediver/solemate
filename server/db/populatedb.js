380588fe2f0cee0453ba8f7510e5dfbf1b350996
lukasz-mac <luka.koscik@gmail.com>
2024-09-30 15:04:41 +0200

    force vercel redeploy

--- server/db/populatedb.js
+++ server/db/populatedb.js
@@ -26,9 +26,9 @@ const SQL = ` CREATE TABLE IF NOT EXISTS shoes (
 
     `;
 
-const host = process.env.POSTGRES_HOST;
 const user = process.env.POSTGRES_USER;
 const password = process.env.POSTGRES_PASSWORD;
+const host = process.env.POSTGRES_HOST;
 const db = process.env.POSTGRES_DATABASE;
 const port = process.env.POSTGRES_PORT;
 
