b1b0d57f894aa8b29e167eeb4e0002fdfe41a2a8
lukasz-mac <luka.koscik@gmail.com>
2024-09-30 16:08:56 +0200

    check vercel build

--- server/db/populatedb.js
+++ server/db/populatedb.js
@@ -34,6 +34,9 @@ const port = process.env.POSTGRES_PORT;
 const ssl = process.env.POSTGRES_SSL ?? '';
 
 async function main() {
+    console.log(
+        `postgresql://${user}:${password}@${host}:${port}/${db}${ssl ? `?sslmode=${ssl}` : ''}`,
+    );
     const myClient = new Client({
         connectionString: `postgresql://${user}:${password}@${host}:${port}/${db}${ssl ? `?sslmode=${ssl}` : ''}`,
     });
